f873fb73ab8a8b91a32032c2fedb6432c0544103
abdel <abdel@localhost.localdomain>
2013-09-09 23:25:58 +0100

    primary job, sample entities with generated session bean and simplified controllers for crud operations

--- src/main/java/com/sos/mabs/samplejeeapp/controllers/DegreeController.java
+++ src/main/java/com/sos/mabs/samplejeeapp/controllers/DegreeController.java
@@ -10,6 +10,10 @@ import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
 import java.util.List;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
 import javax.inject.Inject;
 
 /**
@@ -71,5 +75,48 @@ public class DegreeController implements Serializable {
         this.newDegree = newDegree;
     }
     
+    public Degree getDegree(java.lang.Integer id) {
+        return degreeService.find(id);
+    }
+    
+     @FacesConverter(forClass = Degree.class)
+    public static class DegreeControllerConverter implements Converter {
+
+        @Override
+        public Object getAsObject(FacesContext facesContext, UIComponent component, String value) {
+            if (value == null || value.length() == 0) {
+                return null;
+            }
+            DegreeController controller = (DegreeController) facesContext.getApplication().getELResolver().
+                    getValue(facesContext.getELContext(), null, "degreeController");
+            return controller.getDegree(getKey(value));
+        }
+
+        java.lang.Integer getKey(String value) {
+            java.lang.Integer key;
+            key = Integer.valueOf(value);
+            return key;
+        }
+
+        String getStringKey(java.lang.Integer value) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(value);
+            return sb.toString();
+        }
+
+        @Override
+        public String getAsString(FacesContext facesContext, UIComponent component, Object object) {
+            if (object == null) {
+                return null;
+            }
+            if (object instanceof Degree) {
+                Degree o = (Degree) object;
+                return getStringKey(o.getIdDegree());
+            } else {
+                throw new IllegalArgumentException("object " + object + " is of type " + object.getClass().getName() + "; expected type: " + Degree.class.getName());
+            }
+        }
+    
+}
     
 }
--- src/main/java/com/sos/mabs/samplejeeapp/controllers/PersonController.java
+++ src/main/java/com/sos/mabs/samplejeeapp/controllers/PersonController.java
@@ -5,7 +5,9 @@
 package com.sos.mabs.samplejeeapp.controllers;
 
 import com.sos.mabs.samplejeeapp.entities.Person;
+import com.sos.mabs.samplejeeapp.entities.Qualification;
 import com.sos.mabs.samplejeeapp.services.PersonFacade;
+import com.sos.mabs.samplejeeapp.services.QualificationFacade;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
@@ -22,6 +24,11 @@ public class PersonController implements Serializable {
 
     @Inject
     private PersonFacade personService;
+    @Inject
+    private QualificationFacade qualificationService;
+    
+    private Qualification newQualification;
+    private List<Qualification> qualifications;
     
     private Person person;
     private Person newPerson;
@@ -44,6 +51,12 @@ public class PersonController implements Serializable {
         return "view?faces-redirect=true";
     }
     
+    public String showAddQualification(){
+        newQualification = new Qualification();
+        return "addQualification?faces-redirect=true";
+    }
+    
+    
     public List<Person> getAll(){
         return personService.findAll();
     }
@@ -53,6 +66,14 @@ public class PersonController implements Serializable {
         personService.create(newPerson);
         return this.showList();
     }
+    
+    public String doAddQualification(){
+        newQualification.setPerson(person);
+        qualificationService.create(newQualification);
+        person.getQualificationList().add(newQualification);
+        return "view?faces-redirect=true";
+    }
+    
     
     public Person getPerson() {
         return person;
@@ -69,6 +90,15 @@ public class PersonController implements Serializable {
     public void setNewPerson(Person newPerson) {
         this.newPerson = newPerson;
     }
+
+    public Qualification getNewQualification() {
+        return newQualification;
+    }
+
+    public void setNewQualification(Qualification newQualification) {
+        this.newQualification = newQualification;
+    }
+    
     
     
 }
